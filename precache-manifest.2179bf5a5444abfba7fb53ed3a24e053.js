4f3b26beaa7483b9acfb8be9eda27403a12b3c3c
talkpik <talkpik2020@gmail.com>
2020-10-10 00:44:38 -0700

    Updates

---
+++ precache-manifest.2179bf5a5444abfba7fb53ed3a24e053.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c50d81f56f42468948a48a7aefba729",
+    "url": "/samplelive_public/index.html"
+  },
+  {
+    "revision": "2b7a47306bdf5dbd9393",
+    "url": "/samplelive_public/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e010c052a1bd55eb7af8",
+    "url": "/samplelive_public/static/js/2.aa2163fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/samplelive_public/static/js/2.aa2163fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b7a47306bdf5dbd9393",
+    "url": "/samplelive_public/static/js/main.51a1301b.chunk.js"
+  },
+  {
+    "revision": "04d44d608ea431a22632",
+    "url": "/samplelive_public/static/js/runtime-main.7603a23e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/samplelive_public/static/media/logo.5d5d9eef.svg"
+  }
+]);
